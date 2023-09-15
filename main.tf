5f97c0237a4467ee7bbdde435939fcaad8e2b622
rajiv siddiqui <rajivsiddiqui@rajiv.com>
2023-09-15 12:26:57 +0600

    initial upload to git

---
+++ main.tf
@@ -0,0 +1,209 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+### 1. Create vpc
+
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-04cb4ca688797756f"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "a1"
+#   }  
+# }
+
+### 2. Create Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+}
+
+# # 3. Create Custom Route Table
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Prod"
+  }
+}
+
+# # 4. Create a Subnet 
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+# # 5. Associate subnet with Route Table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# # 6. Create Security Group to allow port 22,80,443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+### 7. Create a network interface with an ip in the subnet that was created in step 4
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+### 8. Assign an elastic IP to the network interface created in step 7
+
+resource "aws_eip" "one" {
+  #vpc                       = true
+  domain                    = "vpc"
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+  depends_on                = [aws_internet_gateway.gw]
+
+  
+  #network_interface         = aws_network_interface.multi-ip.id
+  #associate_with_private_ip = "10.0.0.10"
+}
+
+# output "server_public_ip" {
+#   value = aws_eip.one.public_ip
+# }
+
+### 9. Create Ubuntu server and install/enable apache2
+
+# resource "aws_instance" "web-server-instance" {
+#   ami               = "ami-085925f297f89fce1"
+#   instance_type     = "t2.micro"
+#   availability_zone = "us-east-1a"
+#   key_name          = "rajiv_n_virgina"
+
+#   network_interface {
+#     device_index         = 0
+#     network_interface_id = aws_network_interface.web-server-nic.id
+                           
+#   }
+
+#   user_data = <<-EOF
+#                 #!/bin/bash
+#                 sudo apt update -y
+#                 sudo apt install apache2 -y
+#                 sudo systemctl start apache2
+#                 sudo bash -c 'echo My very web server, Rajiv Siddiqui > /var/www/html/index.html'
+#                 EOF
+#   tags = {
+#     Name = "web-server"
+#   }
+# }
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.web.id
+  allocation_id = aws_eip.example.id
+}
+
+resource "aws_instance" "web" {
+  ami               = "ami-0f844a9675b22ea32"
+  availability_zone = "us-east-1a"
+  instance_type     = "t2.micro"
+  key_name          = "rajiv_n_virgina"
+network_interface {
+  device_index         = 0
+  network_interface_id = aws_network_interface.web-server-nic.id
+                           
+  }
+  user_data = <<-EOF
+                #!/bin/bash
+                # Use this for your user data (script from top to bottom)
+                # install httpd (Linux 2 version)
+                yum update -y
+                yum install -y httpd
+                systemctl start httpd
+                systemctl enable httpd
+                echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+                EOF
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_eip" "example" {
+  domain = "vpc"
+}
+
+
+# output "server_private_ip" {
+#   value = aws_instance.web-server-instance.private_ip
+
+# }
+
+# output "server_id" {
+#   value = aws_instance.web-server-instance.id
+# }
+
+# resource "<provider>_<resource_type>" "name" {
+#     config options.....
+#     key = "value"
+#     key2 = "another value"
+# }
